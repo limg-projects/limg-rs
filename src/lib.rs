9d418125582155e2f2463e766de833521f7a1fce
limg.projects <limg.github@gmail.com>
2025-04-20 00:22:05 +0900

    不要な関数を削除

--- src/lib.rs
+++ src/lib.rs
@@ -11,18 +11,3 @@ pub use limg_core::PixelEndian;
 pub use pixel::Pixel;
 pub use image::Image;
 pub use error::{Error, Result};
-
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
